34c83900627556f54ddaa1950bdb078ec9516b2e
Shane Dyer <shane.dyer01@gmail.com>
2020-06-18 14:15:00 +1200

    Add Header to Layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Header } from 'components';
 
 const Layout = ({
   children,
@@ -13,6 +14,7 @@ const Layout = ({
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
+    <Header />
     <main role="main">{children}</main>
   </>
 );
