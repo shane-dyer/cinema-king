32603189b4cc206586ccec4457a05af7cac34e2a
Shane Dyer <shane.dyer01@gmail.com>
2020-06-18 23:28:51 +1200

    Correct container and grid sizes

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,14 +1,17 @@
+import { Container } from 'components';
 import styles from './styles.module.scss';
 
 const Banner = ({ title, category }) => {
   return (
     <section className={styles.banner}>
-      {title && <h1>{title}</h1>}
-      {category && (
-        <div className={styles.category}>
-          <span>{category}</span>
-        </div>
-      )}
+      <Container>
+        {title && <h1>{title}</h1>}
+        {category && (
+          <div className={styles.category}>
+            <span>{category}</span>
+          </div>
+        )}
+      </Container>
     </section>
   );
 };
