3b3d24095f74385a38a750219fdae41be8f41802
Shane Dyer <shane.dyer01@gmail.com>
2020-06-18 21:10:41 +1200

    Fetch movie list

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
 import { Layout, Banner, Intro } from 'components';
 
-const Index = ({ title, description, ...props }) => (
+const apiUrl = process.env.API_URL;
+
+const Index = ({ movies, title, description, ...props }) => (
   <Layout title={title} description={description}>
     <Banner title="Sci-fi Movies of the decade" category="Feature" />
     <Intro />
@@ -12,8 +14,12 @@ export default Index;
 export async function getStaticProps() {
   const configData = await import('../../siteconfig.json');
 
+  const response = await fetch(apiUrl);
+  const { data } = await response.json();
+
   return {
     props: {
+      movies: data.movies,
       title: configData.default.title,
       description: configData.default.description,
     },
