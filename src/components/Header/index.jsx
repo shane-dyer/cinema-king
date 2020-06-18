96790852feb1dfd132648a27d6ebf54afb02a9f7
Shane Dyer <shane.dyer01@gmail.com>
2020-06-18 15:57:44 +1200

    Add NavBar component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,37 @@
-import { Logo } from 'components';
+import { Logo, NavBar } from 'components';
 import styles from './styles.module.scss';
+
+const NavItems = [
+  {
+    id: 1,
+    title: 'Movies',
+  },
+  {
+    id: 2,
+    title: 'Games',
+  },
+  {
+    id: 3,
+    title: 'Podcasts',
+  },
+  {
+    id: 4,
+    title: 'Awards',
+  },
+  {
+    id: 5,
+    title: 'Shopping',
+  },
+  {
+    id: 6,
+    title: 'Win',
+  },
+];
 
 const Header = () => (
   <header className={styles.header} role="header">
     <Logo />
+    <NavBar items={NavItems} />
   </header>
 );
 
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import styles from './styles.module.scss';
+
+const NavBar = ({ items }) => {
+  if (!Array.isArray(items) || !items.length) return null;
+
+  return (
+    <nav role="navigation">
+      <ul className={styles.list}>
+        {items.map(({ id, title }) => (
+          <li key={id}>
+            <Link href={`#${title.toLowerCase()}`}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
