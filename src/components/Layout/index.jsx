d256d0fff19bcfa2bf90716c3dbcaa41036c45eb
Shane Dyer <shane.dyer01@gmail.com>
2020-06-18 23:29:38 +1200

    Tweak layout site data to be more descriptive

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,18 +3,17 @@ import { Header } from 'components';
 
 const Layout = ({
   children,
-  title = 'CinemaKing',
+  siteTitle = 'CinemaKing',
   description = 'CinemaKing - The leading movie news outlet.',
-  ...props
 }) => (
   <>
     <Head>
       <meta charSet="utf-8" />
-      <title>{title}</title>
+      <title>{siteTitle}</title>
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Caladea&family=Poppins:ital,wght@0,400;0,600;0,700;0,900;1,900&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Caladea&family=Poppins:ital,wght@0,200;0,400;0,600;0,700;0,900;1,900&display=swap"
         rel="stylesheet"
       />
     </Head>
