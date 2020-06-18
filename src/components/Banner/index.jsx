c16444bd569bc99d62f87a8dc585fb8c2377f44d
Shane Dyer <shane.dyer01@gmail.com>
2020-06-18 23:32:19 +1200

    OCD tweaks

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,19 +1,17 @@
 import { Container } from 'components';
 import styles from './styles.module.scss';
 
-const Banner = ({ title, category }) => {
-  return (
-    <section className={styles.banner}>
-      <Container>
-        {title && <h1>{title}</h1>}
-        {category && (
-          <div className={styles.category}>
-            <span>{category}</span>
-          </div>
-        )}
-      </Container>
-    </section>
-  );
-};
+const Banner = ({ title, category }) => (
+  <section className={styles.banner}>
+    <Container>
+      {title && <h1>{title}</h1>}
+      {category && (
+        <div className={styles.category}>
+          <span>{category}</span>
+        </div>
+      )}
+    </Container>
+  </section>
+);
 
 export default Banner;
