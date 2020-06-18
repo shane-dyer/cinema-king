c06e0e6045c01567ad70d580eeb9d5c72635e14b
Shane Dyer <shane.dyer01@gmail.com>
2020-06-18 16:40:13 +1200

    Remove self-hosted fonts and use Google fonts

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -13,6 +13,10 @@ const Layout = ({
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Caladea&family=Poppins:ital,wght@0,400;0,600;0,700;0,900;1,900&display=swap"
+        rel="stylesheet"
+      />
     </Head>
     <Header />
     <main role="main">{children}</main>
