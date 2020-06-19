115a20d6522eecadf3054e5b76e1484c551ea0ed
Shane Dyer <shane.dyer01@gmail.com>
2020-06-19 12:19:12 +1200

    Add movie tile hover effect

---
+++ src/components/MovieTile/HoverIcon.jsx
@@ -0,0 +1,32 @@
+const HoverIcon = () => (
+  <svg width="115" height="115" viewBox="0 0 115 115" fill="none">
+    <circle
+      cx="57.5"
+      cy="57.5"
+      r="56"
+      stroke="white"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+    <line
+      x1="58.5"
+      y1="30.5"
+      x2="58.5"
+      y2="84.5"
+      stroke="white"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+    <line
+      x1="84.5"
+      y1="58.5"
+      x2="30.5"
+      y2="58.5"
+      stroke="white"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+export default HoverIcon;
--- src/components/MovieTile/index.jsx
+++ src/components/MovieTile/index.jsx
@@ -1,8 +1,12 @@
+import HoverIcon from './HoverIcon';
 import styles from './styles.module.scss';
 
 const MovieTile = ({ name, release_date, post_url, index }) => (
   <article className={styles.tile}>
     <div className={styles.image}>
+      <div className={styles.overlay}>
+        <HoverIcon />
+      </div>
       <img src={post_url} alt={name} />
     </div>
     <div className={styles.index}>{index}.</div>
